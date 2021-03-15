0ab0e9ae05ce4fc0f38e5ce57445c32354fa881a
Anthony Rodríguez N <rodriguezanthony981@gmail.com>
2021-03-15 00:52:32 -0500

    Bases completadas

--- bot.py
+++ bot.py
@@ -4,12 +4,28 @@ Basic example for a bot that uses inline keyboards.
 from telegram.ext import Updater, CommandHandler
 from helper import gsheet_helper
 
-from cfg import TOKEN
+from cfg import TOKEN    
     
     #updater = Updater("1595242339:AAFfNxwj3JB108952Oo1jO6VcKmKpYIDURk")
+    #$env:TOKEN="1595242339:AAFfNxwj3JB108952Oo1jO6VcKmKpYIDURk"  <-- copiar
+
+def start(update, context):
+    print(update.message)
+
+    update.message.replay_text(f"Holis! Esto es SkyTravelAPP!")
 
 def main():
-    pass
+    updater = Updater(TOKEN, use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+
+    # Start
+    updater.start_polling()
+    print("Estoy vivo")
+
+    #Me quedo esperando
+    updater.idle()
 
 if __name__=="__main__":
     main()
+    
