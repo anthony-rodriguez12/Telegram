3f5b3ec3e8eef3b724836ed2d18fb76d93f71f4a
Anthony Rodríguez N <rodriguezanthony981@gmail.com>
2021-03-16 12:09:37 -0500

    Base para la fusión

--- bot.py
+++ bot.py
@@ -11,46 +11,49 @@ from cfg import TOKEN
 gsconn = gsheet_helper()
 
 def start(update, context):
-    print(update.message)
+   # print(update.message)
     name = update.message.from_user.username
     Saludo = f"""
-Hola! {name}!, Esto es SkyTravelAPP!\n¿En que puedo ayudarte?\n
-/Ver_Vuelos - Mostrara los Vuelos.\n
-  /Añadir  -  Añadir Nuevo Vuelo
-
+Hola! {name}!, Esto es SkyTravelAPP!\n¿En que puedo ayudarte?
+/Ver_Vuelos - Mostrara los Vuelos.   /Buscar para buscar
     """
     update.message.reply_text(Saludo)
-
-def Añadir_Vuelo(update, context):
-    New_Avi = {
-        'Id': 2,
-        'Avion':"Fenix",
-        'IATA':"GYE",
-        'Aerop - Origen:':"José Joaquín de Olmedo",
-        'Aerop - Destino:':"José Delgado",
-        'Pais-Origen:':"Ecuador",
-        'Pais-Des:':"Mexico",
-        'Ciudad-Orig:':"Guayaquil",
-        'Ciudad-Des:':"Mexico City",
-        'Asientos-Libre:':300,
-        'Asient-Ocupa:': 0
-    }
-    
-    gsconn.store_user(New_Avi)
-    update.message.reply_text("Listo! Datos añadidos!")
-    
+    print("esto es message.text: %s",update.message.text)
+    print("esto es from_user: %s",update.message.from_user)
     
 def listadoV(update, context):
     vuelos = gsconn.getlistado()
     update.message.reply_text(f"{vuelos}")
-
+    # New_Avi = {
+    #     'Id:': 2,
+    #    'Avion:':"Fenix",
+    #    'IATA:':"GYE",
+    #    'Aerop - Origen:':"José Joaquín de Olmedo",
+    #    'Aerop - Destino:':"José Delgado",
+    #    'Pais-Origen:':"Ecuador",
+    #    'Pais-Des:':"Mexico",
+    #    'Ciudad-Orig:':"Guayaquil",
+    #    'Ciudad-Des:':"Mexico City",
+    #    'Asientos-Libre:':300,
+    #    'Asient-Ocupa:': 0
+    #}
+    update.message.reply_text("Ingresa el aeropuerto ")
+    New_Avi = update.message.text
+    gsconn.store_user(New_Avi)
+    
+    #update.message.reply_text(f"Listo! Datos añadidos!")
+def buscar(update, context):
+    update.message.reply_text("Ingresa el aeropuerto ")
+    New_Avi = update.message.text
+    gsconn.store_user(New_Avi)
+    
 
 def main():
     updater = Updater(TOKEN, use_context=True)
 
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.dispatcher.add_handler(CommandHandler("Ver_Vuelos", listadoV))
-    updater.dispatcher.add_handler(CommandHandler("Añadir_Vuelo", Añadir_Vuelo))
+    updater.dispatcher.add_handler(CommandHandler("Buscar", buscar))
 
     # Start
     updater.start_polling()
--- helper.py
+++ helper.py
@@ -4,7 +4,8 @@ import pandas as pd
 from cfg import GDRIVE_SHEET_KEY
 
 ITEM_SHEET = 'Vuelos'
-CLIENT_SHEET = 'Asientos'
+CLIENT_SHEET = 'Lista'
+TIKET_SHEET = 'Asientos'
 
 CREDS_JSON = 'access-key.json'
 
@@ -25,16 +26,31 @@ class gsheet_helper:
 
 
     def getlistado(self):
-        sheet = self.gsheet.worksheet(ITEM_SHEET)
-        Vuelos = pd.DataFrame(sheet.get_all_records())
+        sheetPart = self.gsheet.worksheet(ITEM_SHEET)
+        Parte = pd.DataFrame(sheetPart.get('A1:B11'))
+        sheetComp = self.gsheet.worksheet(CLIENT_SHEET)
+        Completo = pd.DataFrame(sheetComp.get_all_records(),index=['','','','','','',''])
         #Vuelos = pd.DataFrame(sheet.get('A4:B14'))
         #p = pd.DataFrame(sheet.get('A1:K2'))
-        return Vuelos
+        return Completo
 
     def store_user(self, New_Avi):
+        
+        print(New_Avi)
         sheet = self.gsheet.worksheet(CLIENT_SHEET)
-        sheet.add_rows(1)
-        sheet.append_row([element for element in New_Avi.values()])
+        items = pd.DataFrame(sheet.get_all_records(),index=['','','','','','',''])
+
+        lista = pd.DataFrame(items)
+        cond = lista[lista["Aerop - Origen:"] == New_Avi].empty
+        
+        if cond:
+            print("El aeropuerto NO esta registrado")
+        else:
+            print("El aeropuerto si existe y es el: %s",New_Avi)
+
+        #sheet = self.gsheet.worksheet(CLIENT_SHEET)
+        #sheet.add_rows(1)
+        #sheet.append_row([element for element in New_Avi.values()])
 
 if __name__ == "__main__":
     print(gsheet_helper().getlistado())
