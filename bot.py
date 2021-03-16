696a636ba08a7add38d8454bb3c45162b82d5941
Anthony Rodríguez N <rodriguezanthony981@gmail.com>
2021-03-15 20:20:45 -0500

    avance

--- bot.py
+++ bot.py
@@ -6,20 +6,51 @@ from helper import gsheet_helper
 
 from cfg import TOKEN    
     
-    #updater = Updater("1595242339:AAFfNxwj3JB108952Oo1jO6VcKmKpYIDURk")
-    #$env:TOKEN="1595242339:AAFfNxwj3JB108952Oo1jO6VcKmKpYIDURk"  <-- copiar
+    #$env:TOKEN="1595242339:AAFfNxwj3JB108952Oo1jO6VcKmKpYIDURk"  <-- copiar Token
+
+gsconn = gsheet_helper()
 
 def start(update, context):
     print(update.message)
+    name = update.message.from_user.username
+    Saludo = f"""
+Hola! {name}!, Esto es SkyTravelAPP!\n¿En que puedo ayudarte?\n
+/Ver_Vuelos - Mostrara los Vuelos.\n
+  /Añadir  -  Añadir Nuevo Vuelo
 
-    #update.message.replay_text(f"Hola! Esto es SkyTravelAPP!")
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    update.message.reply_text('TOdo bien')
+    """
+    update.message.reply_text(Saludo)
+
+def Añadir_Vuelo(update, context):
+    New_Avi = {
+        'Id': 2,
+        'Avion':"Fenix",
+        'IATA':"GYE",
+        'Aerop - Origen:':"José Joaquín de Olmedo",
+        'Aerop - Destino:':"José Delgado",
+        'Pais-Origen:':"Ecuador",
+        'Pais-Des:':"Mexico",
+        'Ciudad-Orig:':"Guayaquil",
+        'Ciudad-Des:':"Mexico City",
+        'Asientos-Libre:':300,
+        'Asient-Ocupa:': 0
+    }
+    
+    gsconn.store_user(New_Avi)
+    update.message.reply_text("Listo! Datos añadidos!")
+    
+    
+def listadoV(update, context):
+    vuelos = gsconn.getlistado()
+    update.message.reply_text(f"{vuelos}")
+
 
 def main():
     updater = Updater(TOKEN, use_context=True)
 
     updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CommandHandler("Ver_Vuelos", listadoV))
+    updater.dispatcher.add_handler(CommandHandler("Añadir_Vuelo", Añadir_Vuelo))
 
     # Start
     updater.start_polling()
--- helper.py
+++ helper.py
@@ -11,7 +11,31 @@ CREDS_JSON = 'access-key.json'
 
 class gsheet_helper:
     def __init__(self):
-        scope = ["https://spreadsheets.google.com/feeds",
+        scopes = ["https://spreadsheets.google.com/feeds",
                  "https://www.googleapis.com/auth/spreadsheets",
                  "https://www.googleapis.com/auth/drive.file",
                  "https://www.googleapis.com/auth/drive"]
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            CREDS_JSON,
+            scopes
+        )
+
+        self.client = gspread.authorize(creds)
+        self.gsheet = self.client.open_by_key(GDRIVE_SHEET_KEY)
+
+
+    def getlistado(self):
+        sheet = self.gsheet.worksheet(ITEM_SHEET)
+        Vuelos = pd.DataFrame(sheet.get_all_records())
+        #Vuelos = pd.DataFrame(sheet.get('A4:B14'))
+        #p = pd.DataFrame(sheet.get('A1:K2'))
+        return Vuelos
+
+    def store_user(self, New_Avi):
+        sheet = self.gsheet.worksheet(CLIENT_SHEET)
+        sheet.add_rows(1)
+        sheet.append_row([element for element in New_Avi.values()])
+
+if __name__ == "__main__":
+    print(gsheet_helper().getlistado())
+    
